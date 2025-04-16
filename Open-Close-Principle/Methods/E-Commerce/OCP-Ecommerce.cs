ddd26c9359fcf9ee1886af49111130808663b57d
Sandip Pokharel <poksandip1@gmail.com>
2025-04-16 23:12:22 +0545

    demo for OCP in ecommerce website

---
+++ Open-Close-Principle/Methods/E-Commerce/OCP-Ecommerce.cs
@@ -0,0 +1,46 @@
+﻿namespace Open_Close_Principle.Methods.E_Commerce
+{
+    public interface IDiscountStrategy
+    {
+        double CalculateDiscount(double price);
+    }
+
+    public class RegularDiscount : IDiscountStrategy
+    {
+        public double CalculateDiscount(double price)
+        {
+            return price * 0.15;
+        }
+    }
+
+    public class PremiumDiscount : IDiscountStrategy
+    {
+        public double CalculateDiscount(double price)
+        {
+            return price * 0.35;
+        }
+    }
+
+    public class NewbieDiscount: IDiscountStrategy
+    {
+        public double CalculateDiscount(double price)
+        {
+            return price * 0.09;
+        }
+    }
+
+    public class DiscountCalculator
+    {
+        private readonly IDiscountStrategy _discountStrategy;
+
+        public DiscountCalculator(IDiscountStrategy discountStrategy)
+        {
+            _discountStrategy = discountStrategy;
+        }
+
+        public double CalculateFinalDiscount(double price)
+        {
+            return _discountStrategy.CalculateDiscount(price);
+        }
+    }
+}
---
+++ Open-Close-Principle/Program.cs
@@ -0,0 +1,45 @@
+﻿using Open_Close_Principle.Methods.E_Commerce;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+    LOOP:
+        Console.WriteLine(@"1. E-commerce System with Open Closed Principle
+2. Notification System Demo with Open Closed Principle");
+        Console.Write("\nEnter the choices: ");
+        int.TryParse(Console.ReadLine(), out int choice);
+        Console.WriteLine("");
+
+        switch (choice)
+        {
+            case 1:
+                var discount = new RegularDiscount();
+                var calc = new DiscountCalculator(discount);
+
+                double discountPrice = calc.CalculateFinalDiscount(100);
+
+                Console.WriteLine("Normal Discount : {0}", discountPrice);
+
+                var premiumDiscount = new PremiumDiscount();
+                calc = new DiscountCalculator(premiumDiscount);
+                discountPrice = calc.CalculateFinalDiscount(100);
+
+                Console.WriteLine("Premium Discount : {0}", discountPrice);
+
+                break;
+            case 2:
+                break;
+            default:
+                Console.WriteLine("Invalid option. Please try again.");
+                break;
+        }
+        Console.Write("\nDo you want to run again? ");
+        char.TryParse(Console.ReadLine(), out char c);
+
+        if (c == 'y')
+        {
+            goto LOOP;
+        }
+    }
+}
