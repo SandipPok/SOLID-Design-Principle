c2ab95cb4a403fd3a9929c6a74afbabffd20bea2
Sandip Pokharel <poksandip1@gmail.com>
2025-04-16 23:28:52 +0545

    demo for ProductFilter with OCP

---
+++ Open-Close-Principle/Methods/Filtering Product/OCP-FilterProduct.cs
@@ -0,0 +1,51 @@
+﻿namespace Open_Close_Principle.Methods.Filtering_Product
+{
+    public class Product
+    {
+        public string Name { get; set; }
+        public string Color { get; set; }
+        public string Size { get; set; }
+    }
+
+    public interface ISpecification<T>
+    {
+        bool IsSatisfied(T item);
+    }
+    public interface IFilter<T>
+    {
+        IEnumerable<T> Filter(IEnumerable<T> items, ISpecification<T> specification);
+    }
+
+    public class ColorSpecification: ISpecification<Product>
+    {
+        private readonly string _color;
+        public ColorSpecification(string color)
+        {
+            _color = color;
+        }
+        public bool IsSatisfied(Product item)
+        {
+            return item.Color == _color;
+        }
+    }
+    public class SizeSpecification:ISpecification<Product>
+    {
+        private readonly double _size;
+
+        public SizeSpecification(double size)
+        {
+            _size = size;
+        }
+        public bool IsSatisfied(Product item)
+        {
+            return item.Size == _size.ToString();
+        }
+    }
+    public class ProductFilter : IFilter<Product>
+    {
+        public IEnumerable<Product> Filter(IEnumerable<Product> items, ISpecification<Product> specification)
+        {
+            return items.Where(p => specification.IsSatisfied(p));
+        }
+    }
+}
--- Open-Close-Principle/Program.cs
+++ Open-Close-Principle/Program.cs
@@ -1,4 +1,5 @@
 ﻿using Open_Close_Principle.Methods.E_Commerce;
+using Open_Close_Principle.Methods.Filtering_Product;
 using Open_Close_Principle.Methods.Notification_System;
 
 class Program
@@ -7,7 +8,8 @@ class Program
     {
     LOOP:
         Console.WriteLine(@"1. E-commerce System with Open Closed Principle
-2. Notification System Demo with Open Closed Principle");
+2. Notification System Demo with Open Closed Principle
+3. Filtering Products Based on Different Criteria with OCP");
         Console.Write("\nEnter the choices: ");
         int.TryParse(Console.ReadLine(), out int choice);
         Console.WriteLine("");
@@ -37,6 +39,21 @@ class Program
                 var smsNotification = new SMSNotification();
                 sender = new NotificationSender(smsNotification);
                 sender.SendNotification("Hello, this is a test SMS notification!");
+                break;
+            case 3:
+                var products = new List<Product>()
+                {
+                    new Product { Name = "Product1", Color = "Red", Size = "M" },
+                    new Product { Name = "Product2", Color = "Blue", Size = "L" },
+                    new Product { Name = "Product3", Color = "White", Size = "S" },
+                    new Product { Name = "Product4", Color = "Green", Size = "M" },
+                    new Product { Name = "Product5", Color = "Pink", Size = "L" }
+                };
+                var filter = new ProductFilter();
+                foreach (var product in filter.Filter(products, new ColorSpecification("Red")))
+                {
+                    Console.WriteLine($"Product Name: {product.Name}, Color: {product.Color}, Size: {product.Size}");
+                }
                 break;
             default:
                 Console.WriteLine("Invalid option. Please try again.");
